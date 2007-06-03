57dd6144cb804fab4bf12aa5868a44896e316bb0
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-06-03 16:46:44 +0000

    Ported to sh.

--- yag/yag-osdl-0.5/run-yag-osdl.sh
+++ yag/yag-osdl-0.5/run-yag-osdl.sh
@@ -1,24 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "$1" == "--debug" ] ; then
-	do_debug=true
+if [ "$1" = "--debug" ] ; then
+	do_debug=0
 	shift 
 else
-	do_debug=false
+	do_debug=1
 fi
 
 
-function DEBUG
+DEBUG()
 # Displays a debug message if debug mode is activated (do_debug=true).
 # Usage : DEBUG "message 1" "message 2" ...
 {
-	[ "$do_debug" == "false" ] || echo "Debug : $*"
+	[ $do_debug -eq 1 ] || echo "Debug : $*"
 }
 
 
-USAGE="Usage : `basename $0` [--debug] [flags to pass to ${YAG_OSDL_EXEC}]\nThis script manages everything so that yag-osdl is successfully run, or informs in case of errors."
+USAGE="
+
+	Usage : `basename $0` [--debug] [flags to pass to ${YAG_OSDL_EXEC}]
+	
+	This script manages everything so that yag-osdl is successfully run, or informs in case of errors."
 
 YAG_ROOT=`dirname $0`
+YAG_OSDL_EXEC=${YAG_ROOT}/"yagosdl.py"
 
 YAG_OSDL_ENV_SCRIPT=${YAG_ROOT}/yag-osdl-environment.sh
 
@@ -27,29 +32,28 @@ if [ ! -f "${YAG_OSDL_ENV_SCRIPT}" ] ; then
 	exit 2
 fi
 
-YAG_OSDL_EXEC=${YAG_ROOT}/"yagosdl.py"
 
-source ${YAG_OSDL_ENV_SCRIPT}
+. ${YAG_OSDL_ENV_SCRIPT}
 
 PIL_MODULE="Image.py"
 
 
 
 if [ ! -x "${YAG_OSDL_EXEC}" ] ; then
-	echo -e "Error, no executable yag-osdl script available (no executable <${YAG_OSDL_EXEC}> found). $USAGE"
+	echo "Error, no executable yag-osdl script available (no executable <${YAG_OSDL_EXEC}> found). $USAGE"
 	exit 4
 fi	
 
 if [ ! -f "${PIL_ROOT}/${PIL_MODULE}" ] ; then
 	# With gentoo : emerge imaging
-	echo -e "Error, no PIL module available (no <${PIL_ROOT}/${PIL_MODULE}> file found). $USAGE"
+	echo "Error, no PIL module available (no <${PIL_ROOT}/${PIL_MODULE}> file found). $USAGE"
 	exit 8
 fi
 
 
 DEBUG "Running $YAG_OSDL_EXEC..."
 
-if [ "$do_debug" == "true" ] ; then
+if [ "$do_debug" -eq 0 ] ; then
 	python -i ${YAG_OSDL_EXEC} $*
 else
 	${YAG_OSDL_EXEC} $*
