72958abb1115eec1d09aad027807a3a97e16edd3
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-03-18 23:20:44 +0100

    Viewer tools updated.

--- yag/handle-image.sh
+++ yag/handle-image.sh
@@ -3,7 +3,7 @@
 # Helper script for annotate-images.sh
 
 if [ ! $# -eq 1 ]; then
-	echo "  $(basename $0): error, $# arguments given instead of 1." 1>&2
+	echo "  $(basename $0): error, $# argument(s) given instead of 1." 1>&2
 	exit 10
 fi
 
@@ -12,20 +12,34 @@ if [ ! -f "$1" ] ; then
 	exit 11
 fi
 
-#viewer_exec="$(which xv 2>/dev/null)"
-viewer_exec="$(which eog 2>/dev/null)"
+# A viewer: is an executable possibly with options:
+if [ -n "${IMAGE_VIEWER}" ] && [ -x "${IMAGE_VIEWER}" ]; then
 
-if [ -x "${viewer_exec}" ]; then
-	# Was for xv: image_viewer="${viewer_exec} -geometry 500"
-	image_viewer="${viewer_exec}"
+	image_viewer="${IMAGE_VIEWER}"
+
 else
-	viewer_exec=$(which geeqie 2>/dev/null)
-	if [ -x "${viewer_exec}" ]; then
-		image_viewer="${viewer_exec} -g"
+
+	#image_exec="$(which xv 2>/dev/null)"
+	image_exec="$(which eog 2>/dev/null)"
+
+	if [ ! -x "${image_exec}" ]; then
+
+		# Was for xv: image_viewer="${viewer_exec} -geometry 500"
+		image_viewer="${image_exec}"
+
 	else
-		echo "  $(basename $0): error, no image viewer found (tried eog and geeqie)." 1>&2
-		exit 32
+
+		viewer_exec="$(which geeqie 2>/dev/null)"
+		if [ -x "${viewer_exec}" ]; then
+			image_viewer="${viewer_exec} -g"
+		else
+			echo "  $(basename $0): error, no image viewer found (tried eog and geeqie)." 1>&2
+			exit 32
+
+		fi
+
 	fi
+
 fi
 
 image_file="$1"
@@ -44,5 +58,5 @@ beginning_of_name="$(basename ${image_file} | sed 's|\.|-|g')"
 
 #echo "First part of name is '${beginning_of_name}'."
 
-$EDITOR ${image_directory}/${beginning_of_name}.txt
-$EDITOR ${image_directory}/${beginning_of_name}.thm
+${EDITOR} "${image_directory}/${beginning_of_name}.txt"
+${EDITOR} "${image_directory}/${beginning_of_name}.thm"
