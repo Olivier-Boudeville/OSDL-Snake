89fdbde9d3de7f17407cc95ef6c7af48fd4ce894
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-04-02 23:22:30 +0200

    Better management of comments.

--- yag/handle-gallery-directory.sh
+++ yag/handle-gallery-directory.sh
@@ -25,8 +25,22 @@ gallery_comment_file="${gallery_directory}/${gallery_comment_filename}"
 
 if [ ! -f "${gallery_comment_file}" ]; then
 
-	echo "# Replace this text with *comments* regarding the gallery in '${gallery_directory}'." > "${gallery_comment_file}"
+	echo "# Replace this text with *comments* regarding the gallery '$(basename ${gallery_directory})' (located in '${gallery_directory}')." > "${gallery_comment_file}"
 
 fi
 
 ${EDITOR} "${gallery_comment_file}"
+
+# The YAG-OSDL Python code will ignore lines starting with '#' afterwards, yet
+# we prefer to avoid leaving text files that contain no information except the
+# placeholder above, so:
+#
+actual_content="$(grep -v '^ *#' "${gallery_comment_file}")"
+
+if [ -z "${actual_content}" ]; then
+
+	/bin/rm -f "${gallery_comment_file}"
+
+	# No else: we leave comments otherwise.
+
+fi
